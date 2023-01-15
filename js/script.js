b96860121ea02be1d58356eba681b9cd257bd219
clarence101701 <clarence.coronell@gmail.com>
2023-01-15 21:55:01 +0800

    create start and rps buttons interface

--- js/script.js
+++ js/script.js
@@ -1,8 +1,51 @@
 const CHOICES = ["ROCK", "PAPER", "SCISSORS"];
+let playerPick ="";
 let playerWin = 0;
 let compWin = 0;
 let drawCounter = 0;
 const MAXROUNDS = 5;
+
+const start = document.querySelector('.start');
+const rock = document.querySelector("button[data-pick='rock']");
+const paper = document.querySelector("button[data-pick='paper']");
+const scissors = document.querySelector("button[data-pick='scissors']");
+const display = document.querySelector(".msg");
+
+rock.addEventListener('click', () => playerPick='rock');
+paper.addEventListener('click', () => playerPick='paper');
+scissors.addEventListener('click', () => playerPick='scissors');
+
+
+start.addEventListener('click', () => {
+    const strtMsg = "Start";
+    const endMsg = "CHOOSE SOMETHING!";
+    if(playerPick === "") {
+        if(start.textContent === strtMsg){
+            console.log("pressed start")
+            start.classList.remove("btn");
+            start.classList.add("error");
+            start.textContent = endMsg;
+        }
+
+        setTimeout(()=> {
+            if(start.textContent === endMsg){
+                start.classList.remove("error");
+                start.classList.add("btn");
+                start.textContent = "Start";
+            }
+        }, 2000);
+    }else{
+        // playRound(playerPick, getComputerChoice);
+        let comp = getComputerChoice();
+        playRound(playerPick, comp);
+    }
+    
+});
+
+
+
+
+
 
 function getComputerChoice(){
     let rand = Math.floor(Math.random() * 3);
@@ -12,20 +55,20 @@ function getComputerChoice(){
     else return CHOICES[2];
 }
 
-function getPlayerChoice(){
-    let isNotValid = true;
-    let choice;
-    while(isNotValid){
+// function getPlayerChoice(){
+//     let isNotValid = true;
+//     let choice;
+//     while(isNotValid){
 
-        choice = prompt("Pick a Weapon(ROCK, PAPER, or SCISSORS): ").toUpperCase();
-        if( (choice === CHOICES[0]) || (choice === CHOICES[1]) || (choice === CHOICES[2]) ){
-            isNotValid = false;
-            return choice;
-        }else {
-            console.error("Invalid Input");
-        }
-    }
-}
+//         choice = prompt("Pick a Weapon(ROCK, PAPER, or SCISSORS): ").toUpperCase();
+//         if( (choice === CHOICES[0]) || (choice === CHOICES[1]) || (choice === CHOICES[2]) ){
+//             isNotValid = false;
+//             return choice;
+//         }else {
+//             console.error("Invalid Input");
+//         }
+//     }
+// }
 
 function playRound(player, comp){
     player = player.toUpperCase();
@@ -38,50 +81,52 @@ function playRound(player, comp){
         )   {
                 if(player === CHOICES[0]) {
                     playerWin++;
-                    console.log("You WIN! ROCK beats SCISSORS.");
+                    msg.textContent = "You WIN! ROCK beats SCISSORS.";
                 }
                 else if(player === CHOICES[1]) {
                     playerWin++;
-                    console.log("You WIN! PAPER beats ROCK.");
+                    msg.textContent = "You WIN! PAPER beats ROCK.";
                 } 
                 else {
                     playerWin++;
-                    console.log("You WIN! SCISSORS beats PAPER.");
+                    msg.textContent = "You WIN! SCISSORS beats PAPER.";
                 } 
             }
     else if(player === comp) {
         if(player === CHOICES[0]) {
             drawCounter++;
-            console.log("DRAW! ROCK does not beat ROCK.");
+            msg.textContent = "DRAW! ROCK does not beat ROCK.";
         }
         else if(player === CHOICES[1]) {
             drawCounter++;
-            console.log("DRAW! PAPER does not beat PAPER.");
+            msg.textContent = "DRAW! PAPER does not beat PAPER.";
         }
         else if(player === CHOICES[2]) {
             drawCounter++;
-            console.log("DRAW! SCISSORS does not beat SCISSORS.");
+            msg.textContent = "DRAW! SCISSORS does not beat SCISSORS.";
         }
     }
     else{
 
         if(comp === CHOICES[0]) {
             compWin++;
-            console.log("You LOSE! ROCK beats SCISSORS.");
+            msg.textContent = "You LOSE! ROCK beats SCISSORS.";
         }
         else if(comp === CHOICES[1]) {
             compWin++;
-            console.log("You LOSE! PAPER beats ROCK.");
+            msg.textContent = "You LOSE! PAPER beats ROCK.";
         } 
         else {
             compWin++;
-            console.log("You LOSE! SCISSORS beats PAPER.");
+            msg.textContent = "You LOSE! SCISSORS beats PAPER.";
         }
 
     }
 }
 
 function game(){
+
+    //add eventlistener to a button to then call playRound()
     for(i = 0; i < MAXROUNDS; i++){
         playRound(getPlayerChoice(), getComputerChoice());
         console.log("PLAYER: " + playerWin);
